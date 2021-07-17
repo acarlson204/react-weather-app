8224b917164756b676100162da9190ced3cdf4f1
Abby Carlson <abbylcarlson@gmail.com>
2021-07-17 15:14:23 -0500

    Updated footer

--- src/Footer.js
+++ src/Footer.js
@@ -15,6 +15,8 @@ export default function Footer() {
             <hr />
             <footer>
                 <div className="text-center mt-3 mb-4">
+                    This project is made with <a href="https://openweathermap.org/">OpenWeatherAPI</a> , hosted with <a href="https://app.netlify.com/teams/acarlson204/overview">Netlify</a> and open sourced on <a href="https://github.com/acarlson204">Github</a>
+                    <br />
                     2021 Abby Carlson. All rights reserved.
                 </div>
             </footer>
